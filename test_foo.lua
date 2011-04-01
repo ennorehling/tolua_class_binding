ffa6c419fdfe28955372445811a1d78363098881
Enno <enno@eressea.de>
2011-04-01 08:20:37 +0200

    add a small set of tests

---
+++ test_foo.lua
@@ -0,0 +1,29 @@
+require "lunit" -- you need to have this in your LUA_PATH
+require "foo"
+
+module("tests.foo", package.seeall, lunit.testcase)
+
+function test_assign()
+    local a = foo.bar()
+    a.value = 42
+    assert_equal(42, a.value)
+end    
+
+function test_foo()
+    local a = foo.bar()
+    assert_not_equal(nil, a)
+    local b = foo.bar()
+    assert_equal(0, a.value)
+    assert_equal(1, b.value)
+    b = nil
+    collectgarbage()
+    b = foo.bar()
+    assert_equal(1, b.value)
+end
+
+lunit.clearstats()
+local stats = lunit.main({})
+if stats.errors > 0 or stats.failed > 0 then
+    return 1
+end
+
